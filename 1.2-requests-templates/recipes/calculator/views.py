c866fcaec64ba2970d06c9b13f2d1d441a323515
Oskar30 <klim_ak@mail.ru>
2023-07-24 22:28:08 +0400

    done recipe

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,21 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def recipe(request, recipe):
+    if recipe in DATA:
+        servings = request.GET.get('servings', 1)
+        data = DATA[recipe].copy()
+
+        for ingredient, amount in data.items():
+            data[ingredient] = amount * int(servings)
+
+        context = {
+            'recipe':
+               data
+        }
+    
+        return render(request, 'calculator/index.html', context)
+    else:
+        return render(request, 'calculator/index.html')
