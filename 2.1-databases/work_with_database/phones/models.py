e6fe1915ccaab3e82194e53bd19e07d5d9216ac5
Oskar30 <klim_ak@mail.ru>
2023-07-27 17:34:28 +0400

    done pagi

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    image = models.ImageField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
